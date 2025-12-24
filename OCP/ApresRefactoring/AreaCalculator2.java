2ea935dc7c8a9e608f4899b0d83ed59fe1f36095
Chelsy Lapa <chelsylapa@gmail.com>
2025-12-25 00:42:38 +0100

    principes SOLID begin

---
+++ OCP/ApresRefactoring/AreaCalculator2.java
@@ -0,0 +1,10 @@
+package ict301.solid.ocp;
+
+public class AreaCalculator2 {
+
+    public double calculateArea(Shape shape) {
+        // Corrected "ealculateArea" to "calculateArea"
+        return shape.calculateArea();
+    }
+
+}
---
+++ OCP/ApresRefactoring/Main.java
@@ -0,0 +1,11 @@
+package ict301.solid.ocp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Shape shape = new Rectangle(4, 3);
+
+        // Fixed the string concatenation and quotation marks
+        System.out.println("Area = " + shape.calculateArea());
+    }
+}
